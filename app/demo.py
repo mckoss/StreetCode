9263b480a072ac17c92eb60596ff6976ea776a1e
Mike Koss <mckoss@startpad.org>
2012-01-15 14:04:29 -0800

    Kodiak

--- app/demo.py
+++ app/demo.py
@@ -25,32 +25,32 @@ class DemoDataHandler(webapp.RequestHandler):
           self.response.out.write("<html><body><p>Demo Data Created</p>"+
                                   "<p>Created Client Short Codes: "+client_ids+
                                   "</p></body></html>")
-          
-      def loadClients(self):
-          self.buildKodiac()  
 
-          
-      def buildKodiac(self):    
+      def loadClients(self):
+          self.buildKodiac()
+
+
+      def buildKodiac(self):
           kodiac = main.Client()
           kodiac.set_defaults()
           kodiac.shortCode = 'AB'
-          kodiac.displayName = "Kodiac"
+          kodiac.displayName = "Kodiak"
           kodiac.fullName = "Chris"
           kodiac.imageURL = "/images/clients/Kodiac.png"
           kodiac.sponsor = self.sponsors[0]
-          
-          kodiac_story= "<p>Meet Chris. His friends call him \"Kodiac\".</p>"
+
+          kodiac_story= "<p>Meet Chris. His friends call him \"Kodiak\".</p>"
           kodiac_story+="<p>He has a bear of a personality and always hugs instead of shaking hands.  "
           kodiac_story+="Moved to Seattle this past fall, but have been unable to find a job or steady housing.  "
           kodiac_story+="He is currently living in a men's shelter that doesn't have a kitchen or any refrigeration. "
           kodiac_story+="<p>The food stamps he gets can only be used for dry foods - everything which he has to carry in his backpack.  "
           kodiac_story+="He wants a hot meal and a new winter coat.</p><p> You can help.</p>"
-          
+
           kodiac.story=kodiac_story
-          
+
           kodiac.put()
           self.clients.append(kodiac)
-            
+
       def loadSponsors(self):
           logging.debug('Creating sponsors')
           bread = main.Sponsor()
@@ -60,5 +60,5 @@ class DemoDataHandler(webapp.RequestHandler):
           bread.phone= '(206) 682-3579'
           bread.put()
           self.sponsors.append(bread)
-          
-          
+
+

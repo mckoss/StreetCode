1fce263bcd2f82889cbf2a9571472525d767ffc9
Andres Huertas <andre.huertas@gmail.com>
2012-01-14 23:14:18 -0800

    Added client card view and handler

--- app/js/client.js
+++ app/js/client.js
@@ -42,5 +42,40 @@ namespace.module('streetcode.client', function (exports, requires) {
         }
 
     });
+    
+    var ClientMobileView = Backbone.View.extend({
+        el:  "#client-card-view",
+
+        // The DOM events specific to an item.
+        events: {
+            'click .buy': 'buyIt'
+        },
+
+        // The ClientView listens for changes to its model, re-rendering.
+        initialize: function() {
+            // STUB
+            var id = location.pathname.split('/').pop();
+            this.model = new Client({id: id});
+            this.model.bind('change', this.render, this);
+            this.model.fetch();
+        },
+
+        // Re-render the contents of the todo item.
+        render: function() {
+            var dict = this.model.toJSON();
+            var shortCode = encodeURIComponent('http://'+location.hostname+'/1'+dict['shortCode']);
+            var qrUrl = "http://chart.apis.google.com/chart?cht=qr&chs=300x300&chld=H|0&chl="+shortCode;
+            dict['qrUrl']=qrUrl;
+            $(this.el).html(ClientMobileView.template(dict));
+            // Force page to be "re-enhanced" by jQuery mobile
+            $('#client-page').trigger('create');
+            return this;
+        },
+
+        buyIt: function() {
+            alert("buying...");
+        }
+
+    });
 
 }); // seagtug.todos

3a856c0aa35d8118f01ac5df8bbb68b465da0037
Andres Huertas <andre.huertas@gmail.com>
2012-01-16 19:02:44 -0800

    Adding client card html

--- app/js/client.js
+++ app/js/client.js
@@ -94,7 +94,7 @@ namespace.module('streetcode.client', function (exports, requires) {
         render: function() {
             var dict = this.model.toJSON();
             var shortCode = encodeURIComponent('http://'+location.hostname+'/1'+dict['shortCode']);
-            var qrUrl = "http://chart.apis.google.com/chart?cht=qr&chs=300x300&chld=H|0&chl="+shortCode;
+            var qrUrl = "http://chart.apis.google.com/chart?cht=qr&chs=100x100&chld=H|0&chl="+shortCode;
             dict['qrUrl']=qrUrl;
             $(this.el).html(ClientCardView.template(dict));
             // Force page to be "re-enhanced" by jQuery mobile

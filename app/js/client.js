6f59c83fdfafc499565910cce9fb5a3d0b15a5ea
ZepTepi <michaeljackgregory@gmail.com>
2012-03-05 15:29:55 -0800

    css changes - no more reliance on jquery mobile

--- app/js/client.js
+++ app/js/client.js
@@ -6,10 +6,79 @@ namespace.module('streetcode.client', function (exports, requires) {
         'initStory': initStory,
     });
 
+
+  // Page navigation vars
+    var pageWidth = $(window).width();
+    var pageCurr = null;
+    var pagePrev = null;
+    var pageNext = null;
+
+    function debug( p, o) {
+
+      console.log( p + ": " + (o ? (o.attr("id") + " " + o.css("left")): "<>") );
+    }
+
+    function linkHandler(e) {
+        // Prevent clicked A tag from attempting to navigate
+        e.preventDefault();
+
+        // todo: backbone history
+
+        // Send navigation event to Google Analytics
+        _gaq.push(['_trackPageview', location.pathname + location.search + e.target.hash]);
+
+        // grab a handle the # section of the navigation link clicked
+        pageNext = $(e.target.hash);
+
+        console.log("---------------------------");
+        debug("prev", pagePrev );
+        debug("curr", pageCurr );
+        debug("next", pageNext );
+
+        var PageNextToPos;
+        var PageCurrToPos;
+
+        // todo: clean up this page sliding logic
+        if( !pagePrev )
+        {
+                        pageNextToPos = 0;
+                        pageCurrToPos = -1*pageWidth;
+        } else {
+                if( pageNext.attr("id") == pagePrev.attr("id") )
+                {
+                        if( parseInt(pageNext.css("left")) == pageWidth ) {
+                                pageNextToPos = 0;
+                                pageCurrToPos = -1*pageWidth;
+                        } else {
+                                pageNextToPos = 0;
+                                pageCurrToPos = pageWidth;
+                        };
+                } else {
+                        if( parseInt(pagePrev.css("left")) == parseInt(pageNext.css("left"))  ) {
+                                pageNextToPos = 0;
+                                pageCurrToPos = -1*pageWidth;
+                        } else {
+                                if(parseInt(pageNext.css("left")) == parseInt(pageWidth)) {
+                                        pageNextToPos = 0;
+                                        pageCurrToPos = -1*pageWidth;
+                                } else {
+                                        pageNextToPos = 0;
+                                        pageCurrToPos = pageWidth;
+                                };
+                        };
+                };
+        }
+        pageNext.css("left", pageNextToPos);
+        pageCurr.css("left", pageCurrToPos);
+
+        // update page pointers
+        pagePrev = pageCurr;
+        pageCurr = pageNext;
+    }
+
     function initProfile() {
         ClientMobileView.template =  _.template($('#client-view-template').html());
         exports.app = new ClientMobileView();
-
     }
 
     function initCard() {
@@ -37,6 +106,7 @@ namespace.module('streetcode.client', function (exports, requires) {
         url: function() {return '/data/client/' + this.id}
     });
 
+
     var ClientMobileView = Backbone.View.extend({
         el:  "#page-container",
 
@@ -61,9 +131,29 @@ namespace.module('streetcode.client', function (exports, requires) {
         // Re-render the contents of the todo item.
         render: function() {
             $(this.el).append(ClientMobileView.template(this.client));
-            // Force page to be "re-enhanced" by jQuery mobile
-            $('#scan-landing').trigger('create');
-            $.mobile.changePage("#scan-landing");
+
+            // resize pages on window resize;
+            // todo: adjust pagePrev offset on resize
+            $(window).resize(function() {
+              pageWidth = $(window).width();
+            });
+
+            // initialize pages and bind linkHandler to links
+            $(".page").css("left", pageWidth);
+            $(".page").css("top", 65);
+            $(".page").addClass("transform");
+            $("A").bind("click", function(e) {
+              linkHandler(e);
+            });
+
+            // hide Loading panel
+            $("#loading").remove();
+
+            // load initial page
+            // todo: look into a more appropriate way to select first page
+            pageCurr = $(".page:eq(0)");
+            pageCurr.css("left",0);
+
             return this;
         },
 
@@ -97,8 +187,6 @@ namespace.module('streetcode.client', function (exports, requires) {
             var qrUrl = "http://chart.apis.google.com/chart?cht=qr&chs=100x100&chld=H|0&chl="+shortCode;
             dict['qrUrl']=qrUrl;
             $(this.el).html(ClientCardView.template(dict));
-            // Force page to be "re-enhanced" by jQuery mobile
-            $('#client-page').trigger('create');
             return this;
         },
 
@@ -132,8 +220,6 @@ namespace.module('streetcode.client', function (exports, requires) {
             var qrUrl = "http://chart.apis.google.com/chart?cht=qr&chs=500x500&chld=H|0&chl="+shortCode;
             dict['qrUrl']=qrUrl;
             $(this.el).html(ClientSignView.template(dict));
-            // Force page to be "re-enhanced" by jQuery mobile
-            $('#client-page').trigger('create');
             return this;
         },
 
@@ -167,8 +253,6 @@ namespace.module('streetcode.client', function (exports, requires) {
             var qrUrl = "http://chart.apis.google.com/chart?cht=qr&chs=100x100&chld=H|0&chl="+shortCode;
             dict['qrUrl']=qrUrl;
             $(this.el).html(ClientStoryView.template(dict));
-            // Force page to be "re-enhanced" by jQuery mobile
-            $('#client-page').trigger('create');
             return this;
         },
 
@@ -178,4 +262,4 @@ namespace.module('streetcode.client', function (exports, requires) {
 
     });
 
-}); // seagtug.todos
+});

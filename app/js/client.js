9faddd1c7ceb11a1302a6477b93afc834878c2ba
MikeGregory <michaeljackgregory@gmail.com>
2012-04-09 17:59:55 -0700

    Update app/js/client.js

--- app/js/client.js
+++ app/js/client.js
@@ -1,265 +1,245 @@
 namespace.module('streetcode.client', function (exports, requires) {
-    exports.extend({
-        'initProfile': initProfile,
-        'initCard': initCard,
-        'initSign': initSign,
-        'initStory': initStory,
-    });
+    	exports.extend({
+				'initProfile': initProfile,
+				'initCard': initCard,
+				'initSign': initSign,
+				'initStory': initStory,
+		});
+
+	// Page navigation vars
+		var pageWidth = $(window).width();
+		var pageCurr = null;
+		var pageNext = null;
+
+		function initRouter()
+		{
+				// Initiate the router
+				var router = new AppRouter;
+				// Start Backbone history a neccesary step for bookmarkable URL's
+				Backbone.history.start();
+		}
+
+		var AppRouter = Backbone.Router.extend({
+				routes: {
+					"give/:amount": "give",
+						"*actions": "linkHandler" // matches http://example.com/#anything-here
+				},
+				give: function( amount){
+					alert("setup donation of $" + amount);
+					$("#amount").val(amount);
+					this.linkHandler( "give" );
+				},
+				linkHandler: function( actions ){
+						// grab a handle the # section of the navigation link clicked
+					// if no actions, navigate home
+					// pageNext = actions ? $("#"+actions) : $(".page:eq(0)");
+						var page = (actions) ? actions : "home";
+						page = "#" + page;
+						pageNext = $(page);
+						
+						// Send navigation event to Google Analytics
+						_gaq.push(['_trackPageview', location.pathname + location.search + page]);
+												
+						// slide pages
+						$('div .page').each(function(index) {
+								$(this).css("left",-1*pageWidth);
+								$(this).removeClass("transform");
+						});
+						if(pageCurr) {
+							//alert(pageCurr.css("left"));
+							//pageCurr.css("left", -1*pageWidth);
+							//pageCurr.removeClass("transform");
+						}
+						pageNext.css("left", pageWidth);
+						pageNext.addClass("transform");
+						pageNext.css("left", 0);
+
+						// update page pointers
+						pageCurr = pageNext;
+				}
+		});		 
+
+		function debug( p, o) {
+			console.log( p + ": " + (o ? (o.attr("id") + " " + o.css("left")): "<>") );
+		}
+
+		function initProfile() {
+				ClientMobileView.template =	 _.template($('#client-view-template').html());
+				exports.app = new ClientMobileView();
+		}
+
+		function initCard() {
+				ClientCardView.template =	 _.template($('#client-view-template').html());
+				exports.app = new ClientCardView();
+		}
+
+		function initSign() {
+				ClientSignView.template =	 _.template($('#client-view-template').html());
+				exports.app = new ClientSignView();
+		}
+
+		function initStory() {
+				ClientStoryView.template =	_.template($('#client-view-template').html());
+				exports.app = new ClientStoryView();
+		}
 
 
-  // Page navigation vars
-    var pageWidth = $(window).width();
-    var pageCurr = null;
-    var pagePrev = null;
-    var pageNext = null;
+		var ClientList = Backbone.Collection.extend({
+				model: Client,
+				url: '/data/client'
+		});
 
-    function debug( p, o) {
+		var Client = Backbone.Model.extend({
+				url: function() {return '/data/client/' + this.id}
+		});
 
-      console.log( p + ": " + (o ? (o.attr("id") + " " + o.css("left")): "<>") );
-    }
+		var ClientMobileView = Backbone.View.extend({
+				el:	 "#page-container",
 
-    function linkHandler(e) {
-        // Prevent clicked A tag from attempting to navigate
-        e.preventDefault();
+				// The DOM events specific to an item.
+				events: {
+						'click .buy': 'buyIt'
+				},
 
-        // todo: backbone history
+				// The ClientView listens for changes to its model, re-rendering.
+				initialize: function() {
+						var shortCode = location.pathname.split('/').pop().slice(1);
+						var self = this;
+						$.ajax({
+								url: '/data/client?shortCode=' + shortCode,
+								dataType: 'json',
+								success: function (data) {
+										self.client = data[0];
+										self.render();
+								}});
+				},
 
-        // Send navigation event to Google Analytics
-        _gaq.push(['_trackPageview', location.pathname + location.search + e.target.hash]);
+				// Re-render the contents of the todo item.
+				render: function() {
+						$(this.el).append(ClientMobileView.template(this.client));
 
-        // grab a handle the # section of the navigation link clicked
-        pageNext = $(e.target.hash);
+						// resize pages on window resize;
+						// todo: adjust pagePrev offset on resize
+						$(window).resize(function() {
+							pageWidth = $(window).width();
+						});
 
-        console.log("---------------------------");
-        debug("prev", pagePrev );
-        debug("curr", pageCurr );
-        debug("next", pageNext );
+						// hide Loading panel
+						$("#loading").remove();
 
-        var PageNextToPos;
-        var PageCurrToPos;
+						// initialize pages
+						$("div .page").css("left", pageWidth);
+						$("div .page").css("top", 65);
 
-        // todo: clean up this page sliding logic
-        if( !pagePrev )
-        {
-                        pageNextToPos = 0;
-                        pageCurrToPos = -1*pageWidth;
-        } else {
-                if( pageNext.attr("id") == pagePrev.attr("id") )
-                {
-                        if( parseInt(pageNext.css("left")) == pageWidth ) {
-                                pageNextToPos = 0;
-                                pageCurrToPos = -1*pageWidth;
-                        } else {
-                                pageNextToPos = 0;
-                                pageCurrToPos = pageWidth;
-                        };
-                } else {
-                        if( parseInt(pagePrev.css("left")) == parseInt(pageNext.css("left"))  ) {
-                                pageNextToPos = 0;
-                                pageCurrToPos = -1*pageWidth;
-                        } else {
-                                if(parseInt(pageNext.css("left")) == parseInt(pageWidth)) {
-                                        pageNextToPos = 0;
-                                        pageCurrToPos = -1*pageWidth;
-                                } else {
-                                        pageNextToPos = 0;
-                                        pageCurrToPos = pageWidth;
-                                };
-                        };
-                };
-        }
-        pageNext.css("left", pageNextToPos);
-        pageCurr.css("left", pageCurrToPos);
+						// initialize backbone router
+						initRouter();
+						
+						return this;
+				},
 
-        // update page pointers
-        pagePrev = pageCurr;
-        pageCurr = pageNext;
-    }
+				buyIt: function() {
+						alert("buying...");
+				}
 
-    function initProfile() {
-        ClientMobileView.template =  _.template($('#client-view-template').html());
-        exports.app = new ClientMobileView();
-    }
+		});
 
-    function initCard() {
-        ClientCardView.template =  _.template($('#client-view-template').html());
-        exports.app = new ClientCardView();
-    }
+		var ClientCardView = Backbone.View.extend({
+				el:	 "#client-card-view",
 
-    function initSign() {
-        ClientSignView.template =  _.template($('#client-view-template').html());
-        exports.app = new ClientSignView();
-    }
+				// The DOM events specific to an item.
+				events: {
+						'click .buy': 'buyIt'
+				},
 
-    function initStory() {
-        ClientStoryView.template =  _.template($('#client-view-template').html());
-        exports.app = new ClientStoryView();
-    }
+				// The ClientView listens for changes to its model, re-rendering.
+				initialize: function() {
+						// STUB
+						var id = location.pathname.split('/').pop();
+						this.model = new Client({id: id});
+						this.model.bind('change', this.render, this);
+						this.model.fetch();
+				},
 
+				// Re-render the contents of the todo item.
+				render: function() {
+						var dict = this.model.toJSON();
+						var shortCode = encodeURIComponent('http://'+location.hostname+'/1'+dict['shortCode']);
+						var qrUrl = "http://chart.apis.google.com/chart?cht=qr&chs=100x100&chld=H|0&chl="+shortCode;
+						dict['qrUrl']=qrUrl;
+						$(this.el).html(ClientCardView.template(dict));
+						return this;
+				},
 
-    var ClientList = Backbone.Collection.extend({
-        model: Client,
-        url: '/data/client'
-    });
+				buyIt: function() {
+						alert("buying...");
+				}
 
-    var Client = Backbone.Model.extend({
-        url: function() {return '/data/client/' + this.id}
-    });
+		});
 
+		var ClientSignView = Backbone.View.extend({
+				el:	 "#client-sign-view",
 
-    var ClientMobileView = Backbone.View.extend({
-        el:  "#page-container",
+				// The DOM events specific to an item.
+				events: {
+						'click .buy': 'buyIt'
+				},
 
-        // The DOM events specific to an item.
-        events: {
-            'click .buy': 'buyIt'
-        },
+				// The ClientView listens for changes to its model, re-rendering.
+				initialize: function() {
+						// STUB
+						var id = location.pathname.split('/').pop();
+						this.model = new Client({id: id});
+						this.model.bind('change', this.render, this);
+						this.model.fetch();
+				},
 
-        // The ClientView listens for changes to its model, re-rendering.
-        initialize: function() {
-            var shortCode = location.pathname.split('/').pop().slice(1);
-            var self = this;
-            $.ajax({
-                url: '/data/client?shortCode=' + shortCode,
-                dataType: 'json',
-                success: function (data) {
-                    self.client = data[0];
-                    self.render();
-                }});
-        },
+				// Re-render the contents of the todo item.
+				render: function() {
+						var dict = this.model.toJSON();
+						var shortCode = encodeURIComponent('http://'+location.hostname+'/1'+dict['shortCode']);
+						var qrUrl = "http://chart.apis.google.com/chart?cht=qr&chs=500x500&chld=H|0&chl="+shortCode;
+						dict['qrUrl']=qrUrl;
+						$(this.el).html(ClientSignView.template(dict));
+						return this;
+				},
 
-        // Re-render the contents of the todo item.
-        render: function() {
-            $(this.el).append(ClientMobileView.template(this.client));
+				buyIt: function() {
+						alert("buying...");
+				}
 
-            // resize pages on window resize;
-            // todo: adjust pagePrev offset on resize
-            $(window).resize(function() {
-              pageWidth = $(window).width();
-            });
+		});
 
-            // initialize pages and bind linkHandler to links
-            $(".page").css("left", pageWidth);
-            $(".page").css("top", 65);
-            $(".page").addClass("transform");
-            $("A").bind("click", function(e) {
-              linkHandler(e);
-            });
+		var ClientStoryView = Backbone.View.extend({
+				el:	 "#client-story-view",
 
-            // hide Loading panel
-            $("#loading").remove();
+				// The DOM events specific to an item.
+				events: {
+						'click .buy': 'buyIt'
+				},
 
-            // load initial page
-            // todo: look into a more appropriate way to select first page
-            pageCurr = $(".page:eq(0)");
-            pageCurr.css("left",0);
+				// The ClientView listens for changes to its model, re-rendering.
+				initialize: function() {
+						// STUB
+						var id = location.pathname.split('/').pop();
+						this.model = new Client({id: id});
+						this.model.bind('change', this.render, this);
+						this.model.fetch();
+				},
 
-            return this;
-        },
+				// Re-render the contents of the todo item.
+				render: function() {
+						var dict = this.model.toJSON();
+						var shortCode = encodeURIComponent('http://'+location.hostname+'/1'+dict['shortCode']);
+						var qrUrl = "http://chart.apis.google.com/chart?cht=qr&chs=100x100&chld=H|0&chl="+shortCode;
+						dict['qrUrl']=qrUrl;
+						$(this.el).html(ClientStoryView.template(dict));
+						return this;
+				},
 
-        buyIt: function() {
-            alert("buying...");
-        }
+				buyIt: function() {
+						alert("buying...");
+				}
 
-    });
-
-    var ClientCardView = Backbone.View.extend({
-        el:  "#client-card-view",
-
-        // The DOM events specific to an item.
-        events: {
-            'click .buy': 'buyIt'
-        },
-
-        // The ClientView listens for changes to its model, re-rendering.
-        initialize: function() {
-            // STUB
-            var id = location.pathname.split('/').pop();
-            this.model = new Client({id: id});
-            this.model.bind('change', this.render, this);
-            this.model.fetch();
-        },
-
-        // Re-render the contents of the todo item.
-        render: function() {
-            var dict = this.model.toJSON();
-            var shortCode = encodeURIComponent('http://'+location.hostname+'/1'+dict['shortCode']);
-            var qrUrl = "http://chart.apis.google.com/chart?cht=qr&chs=100x100&chld=H|0&chl="+shortCode;
-            dict['qrUrl']=qrUrl;
-            $(this.el).html(ClientCardView.template(dict));
-            return this;
-        },
-
-        buyIt: function() {
-            alert("buying...");
-        }
-
-    });
-
-    var ClientSignView = Backbone.View.extend({
-        el:  "#client-sign-view",
-
-        // The DOM events specific to an item.
-        events: {
-            'click .buy': 'buyIt'
-        },
-
-        // The ClientView listens for changes to its model, re-rendering.
-        initialize: function() {
-            // STUB
-            var id = location.pathname.split('/').pop();
-            this.model = new Client({id: id});
-            this.model.bind('change', this.render, this);
-            this.model.fetch();
-        },
-
-        // Re-render the contents of the todo item.
-        render: function() {
-            var dict = this.model.toJSON();
-            var shortCode = encodeURIComponent('http://'+location.hostname+'/1'+dict['shortCode']);
-            var qrUrl = "http://chart.apis.google.com/chart?cht=qr&chs=500x500&chld=H|0&chl="+shortCode;
-            dict['qrUrl']=qrUrl;
-            $(this.el).html(ClientSignView.template(dict));
-            return this;
-        },
-
-        buyIt: function() {
-            alert("buying...");
-        }
-
-    });
-
-    var ClientStoryView = Backbone.View.extend({
-        el:  "#client-story-view",
-
-        // The DOM events specific to an item.
-        events: {
-            'click .buy': 'buyIt'
-        },
-
-        // The ClientView listens for changes to its model, re-rendering.
-        initialize: function() {
-            // STUB
-            var id = location.pathname.split('/').pop();
-            this.model = new Client({id: id});
-            this.model.bind('change', this.render, this);
-            this.model.fetch();
-        },
-
-        // Re-render the contents of the todo item.
-        render: function() {
-            var dict = this.model.toJSON();
-            var shortCode = encodeURIComponent('http://'+location.hostname+'/1'+dict['shortCode']);
-            var qrUrl = "http://chart.apis.google.com/chart?cht=qr&chs=100x100&chld=H|0&chl="+shortCode;
-            dict['qrUrl']=qrUrl;
-            $(this.el).html(ClientStoryView.template(dict));
-            return this;
-        },
-
-        buyIt: function() {
-            alert("buying...");
-        }
-
-    });
+		});
 
 });

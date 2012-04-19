f28d28eab079d9eb1418f55ccb3fd3e3a4c33164
Mike Koss <mckoss@startpad.org>
2012-04-18 18:15:58 -0700

    fix syntax errors

--- app/paypal.py
+++ app/paypal.py
@@ -9,15 +9,15 @@ class pdt_handler(webapp.RequestHandler):
     def get(self, client):
         # Get the transaction id, tx=blahblahblah
         trans_id = self.request.get("tx")
-        
+
         # Confgure POST args
         args = {}
         # Specify the paypal command
         args["cmd"] ="_notify-synch"
         args["tx"] = trans_id
         args["at"] = settings.PAYPAL_PDT_KEY
-        
-        args = urllib.urlencode(args) 
+
+        args = urllib.urlencode(args)
 
         try:
             # Do a post back to validate
@@ -26,8 +26,8 @@ class pdt_handler(webapp.RequestHandler):
                                     method = urlfetch.POST,
                                     payload = args).content
         except:
-          self.response.out.write("POST Failed") 
-        
+          self.response.out.write("POST Failed")
+
         # Check for SUCCESS at the start of the response
         if re.search("^SUCCESS", status):
             # Check other transaction details here like
@@ -36,43 +36,32 @@ class pdt_handler(webapp.RequestHandler):
             # TODO Update donation transaction in streetcodes
 
             # redirect user to thank you screen "/client_short_code#thanks"
-            self.response.out.write('<script> window.location = "/'+ client + '#thanks"; </script>') 
-            
-            # DEBUG code 
-            # self.response.out.write("OK<br>") 
+            self.response.out.write('<script> window.location = "/'+ client + '#thanks"; </script>')
+
+            # DEBUG code
+            # self.response.out.write("OK<br>")
             # self.response.out.write(urllib.unquote(status))
         else:
             self.response.out.write("Failed")
 
-class ipn_handler(Endpoint)
-    def process(self, data):
-        return HttpResponse("IPN received")
-        # Do something with valid data from PayPal - e-mail it to yourself,
-        # stick it in a database, generate a license key and e-mail it to the
-        # user... whatever
-        
-    def process_invalid(self, data):
-        # Do something with invalid data (could be from anywhere) - you 
-        # should probably log this somewhere
+class Endpoint(object):
 
-class Endpoint:
-    
     default_response_text = 'Nothing to see here'
     verify_url = settings.PAYPAL_URL # "https://www.paypal.com/cgi-bin/webscr"
-    
+
     def do_post(self, url, args):
         return urllib.urlopen(url, urllib.urlencode(args)).read()
-    
+
     def verify(self, data):
         args = {
             'cmd': '_notify-validate',
         }
         args.update(data)
         return self.do_post(self.verify_url, args) == 'VERIFIED'
-    
+
     def default_response(self):
         return HttpResponse(self.default_response_text)
-    
+
     def __call__(self, request):
         r = None
         if request.method == 'POST':
@@ -86,15 +75,28 @@ class Endpoint:
             return r
         else:
             return self.default_response()
-    
+
     def process(self, data):
         pass
-    
+
     def process_invalid(self, data):
         pass
 
+class ipn_handler(Endpoint):
+    def process(self, data):
+        return HttpResponse("IPN received")
+        # Do something with valid data from PayPal - e-mail it to yourself,
+        # stick it in a database, generate a license key and e-mail it to the
+        # user... whatever
+
+    def process_invalid(self, data):
+        # Do something with invalid data (could be from anywhere) - you
+        # should probably log this somewhere
+        pass
+
+
 class AppEngineEndpoint(Endpoint):
-    
+
     def do_post(self, url, args):
         from google.appengine.api import urlfetch
         return urlfetch.fetch(
